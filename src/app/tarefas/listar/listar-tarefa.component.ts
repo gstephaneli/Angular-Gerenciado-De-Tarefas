699552b3b3d41daf1bcaa3dc3f2be0aceed85cdb
Guilherme Stephaneli <guilhermesthephaneli507@gmail.com>
2020-02-20 13:44:27 -0300

    Adicionando listagem de tarefas no html

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,4 +1,7 @@
+import { Tarefa } from './../shared/tarefa.module';
+import { TarefaService } from './../shared/tarefa.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa[];
+  constructor(private tarefaService: TarefaService) { }
 
+  /*é sempre chamado depois da inicialização de um construtor*/
   ngOnInit(): void {
+    this.tarefas = this.listarTodos();
+
+  }
+
+  listarTodos():Tarefa[]{
+
+    return this.tarefaService.listarTodos();
+  }
+
+  remove( ):void{
+    
   }
 
 }
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,7 +1,9 @@
+import { RouterModule } from '@angular/router';
 import { TarefaService } from './shared/tarefa.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarTarefaComponent } from './listar/listar-tarefa.component';
+import { FormsModule } from '@angular/forms'
 
 
 
@@ -11,7 +13,9 @@ import { ListarTarefaComponent } from './listar/listar-tarefa.component';
     ListarTarefaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers:[TarefaService]
 })
